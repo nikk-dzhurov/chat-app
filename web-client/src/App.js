2857d1bd6cdd248e2e5f49283b5b6a41f53bfc6f
Nikolay Dzhurov <chinzo.nd@gmail.com>
2018-12-22 09:33:29 +0200

    Add material-ui package and upgrade React to alpha version with Hooks proposal

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,11 +1,55 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import PropTypes from 'prop-types';
+
+import Switch from '@material-ui/core/Switch';
+import {MuiThemeProvider, createMuiTheme, withTheme} from '@material-ui/core/styles';
+
+const themes = {
+	light: createMuiTheme({
+		palette: {
+			type: 'light',
+		},
+	}),
+	dark: createMuiTheme({
+		palette: {
+			type: 'dark',
+		},
+	}),
+};
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			currentUser: null,
+			checked: false,
+		};
+	}
+
+	componentDidMount() {
+		document.title = 'Chat App';
+	}
+
 	render() {
 		return (
-			<div>
-				Hello, World!
-			</div>
+			<MuiThemeProvider theme={themes[this.state.themeKey]}>
+				<BarT checked={this.state.checked} onChange={(e, checked) => this.setState({themeKey: checked ? 'light' : 'dark'})} />
+			</MuiThemeProvider>
 		);
 	}
 }
+App.childContextTypes = {
+	currentUser: PropTypes.object,
+};
+
+const Bar = (props, {muiTheme}) => {
+	return (
+		<div style={{background: props.theme.palette.background}}>
+			Choose theme:<br />
+			Dark <Switch checked={props.checked} onChange={props.onChange} /> Light
+		</div>
+	)
+}
+
+const BarT = withTheme()(Bar);
